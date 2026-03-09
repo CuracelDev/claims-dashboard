bdf5ad70d52e2b30e91320839f12d371230a88b6
FadeJnrWar <emmanuel.o@curacel.ai>
2026-03-09 22:24:58 +0100

    fix: remove missing globals.css import

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { DM_Sans } from 'next/font/google';
 import Providers from '@/app/providers';
 import Sidebar from '@/app/components/Sidebar';
-import './globals.css';
 
 const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-dm-sans' });
 
